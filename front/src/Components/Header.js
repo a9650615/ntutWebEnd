13f3e7c07f1c5bc690b684475515f2bec532a008
菜鳥祐 <a9650615@gmail.com>
2017-05-25 10:08:22 +0800

    登入/登出 頁面 圖示

--- front/src/Components/Header.js
+++ front/src/Components/Header.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react'
 import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
+import IconButton from 'material-ui/IconButton'
 
 export default class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      drawerOpen: false
+    }
+  }
+
+  drawerSw() {
+    this.setState({
+      drawerOpen: !this.state.drawerOpen
+    })
+  }
+
+  rightButtons() {
+    return (
+      <div>
+        <IconButton iconClassName="material-icons">face</IconButton>
+        <IconButton iconClassName="material-icons">&#xE8AF;</IconButton>
+        <IconButton iconClassName="material-icons">&#xE5C5;</IconButton>
+      </div>
+    )
+  }
+
   render() {
     return (
-      <AppBar
-        title="Fakebook"
-        iconClassNameRight="muidocs-icon-navigation-expand-more"
-      />
+      <div>
+        <AppBar
+          title="Fakebook"
+          onLeftIconButtonTouchTap={this.drawerSw.bind(this)}
+          iconElementRight={this.rightButtons()}
+          />
+        <Drawer 
+          open={this.state.drawerOpen} 
+          docked={false}
+          width={300}
+          onRequestChange={(data) => this.setState({drawerOpen: data})}>
+
+        </Drawer>
+      </div>
     );
   }
 }
