23432e4bd33bfeb35d1d41e61c3a23645b041cae
菜鳥祐 <a9650615@gmail.com>
2017-06-19 02:57:10 +0800

    footer 樣式修正

--- front/src/Components/Footer/index.js
+++ front/src/Components/Footer/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Grid, Row, Col } from 'react-flexbox-grid'
+import {Link} from 'react-router-dom'
 import './Footer.css'
 
 class Footer extends Component {
@@ -8,14 +9,19 @@ class Footer extends Component {
       <div className="footer">
         <Grid fluid>
           <Row>
-            <Col sm={6}>
-              title
+            <Col sm={3}>
+              <img src="/img/logo.png" alt="" style={{width: '100%'}}/>
             </Col>
-            <Col sm={6}>
-              網站連結
+            <Col smOffset={6} sm={3}>
+              <p><Link to="/intruction">美好世界</Link></p>
+              <p><Link to="/creator">創世神</Link></p>
             </Col>
           </Row>
           <hr />
+          <Row>
+            <Col xs={12} sm={6}>只有愛心不被冷漠，為你為台灣</Col>
+            <Col xs={12} sm={6} className="textRight">CopyRight@2017.Aqua</Col>
+          </Row>
         </Grid>
       </div>
     )
