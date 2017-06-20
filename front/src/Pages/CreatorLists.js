ed4652077f4004dc4d2f98f427318a772edc8727
enchang <enchang1017@gamil.com>
2017-06-21 02:06:44 +0800

    新增圖片

--- front/src/Pages/CreatorLists.js
+++ front/src/Pages/CreatorLists.js
@@ -9,9 +9,9 @@ class CreatorLists extends Component {
       <div>
         <Grid fluid>
           <CreatorInfo img="/img/enChen.jpg" name="許景程" content={"理念擔當-創世神 Demiurge"} mdFile="EnChen.md"/>
-          <CreatorInfo name="蔡銘祐" content={"程式擔當-造物者 Creater"} mdFile="MingYou.md"/>
-          <CreatorInfo name="彭旭豪" content={"美術擔當-雕塑者 Sculptor"} mdFile="HowLemon.md"/>
-          <CreatorInfo name="林軒弘" content={"執行擔當-奔波者 Rider"} mdFile="Mental.md"/>
+          <CreatorInfo img="/img/birdyo.jpg" name="蔡銘祐" content={"程式擔當-造物者 Creater"} mdFile="MingYou.md"/>
+          <CreatorInfo img="/img/dead.jpg" name="彭旭豪" content={"美術擔當-雕塑者 Sculptor"} mdFile="HowLemon.md"/>
+          <CreatorInfo img="/img/rider.jpg" name="林軒弘" content={"執行擔當-奔波者 Rider"} mdFile="Mental.md"/>
         </Grid>
       </div>
     )
